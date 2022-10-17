48456aa7c85db29c8895e5ad78c3eb17e03e1b2f
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2022-10-17 22:25:12 +0700

    fix theme kanban UI

--- src/layouts/components/Card/Card.js
+++ src/layouts/components/Card/Card.js
@@ -8,15 +8,14 @@ const cx = classNames.bind(styles);
 function Card({ card }) {
     return (
         <div className={cx('wrapper')}>
-            {card.picture && <img src={card.picture} alt="Cover" className={cx('picture')} />}
+            <span className={cx('tag', card.tagColor)}></span>
             <header className={cx('header')}>
                 <span className={cx('label')}>{card.label}</span>
                 <span className={cx('date')}>{card.createdAt}</span>
             </header>
-            <div className={cx('card-info')}>
-                <span className={cx('title')}>{card.title}</span>
-                <p className={cx('desc')}>{card.description}</p>
-            </div>
+            <span className={cx('title')}>{card.title}</span>
+            {card.picture && <img src={card.picture} alt="Cover" className={cx('picture')} />}
+            <p className={cx('desc')}>{card.description}</p>
         </div>
     );
 }
