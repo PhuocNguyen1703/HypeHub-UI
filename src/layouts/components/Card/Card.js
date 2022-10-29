6ec4498d71e63e708cf43b9f762276a0c3331cad
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2022-10-29 15:02:20 +0700

    update UI

--- src/layouts/components/Card/Card.js
+++ src/layouts/components/Card/Card.js
@@ -8,14 +8,16 @@ const cx = classNames.bind(styles);
 function Card({ card }) {
     return (
         <div className={cx('wrapper')}>
-            <span className={cx('tag', card.tagColor)}></span>
-            <header className={cx('header')}>
-                <span className={cx('label')}>{card.label}</span>
-                <span className={cx('date')}>{card.createdAt}</span>
-            </header>
-            <span className={cx('title')}>{card.title}</span>
-            {card.picture && <img src={card.picture} alt="Cover" className={cx('picture')} />}
-            <p className={cx('desc')}>{card.description}</p>
+            <span className={cx('tag')}></span>
+            <div className={cx('container')}>
+                <header className={cx('header')}>
+                    <span className={cx('label', card.tagColor)}>{card.label}</span>
+                    <span className={cx('date')}>{card.createdAt}</span>
+                </header>
+                <span className={cx('title')}>{card.title}</span>
+                {card.picture && <img src={card.picture} alt="Cover" className={cx('picture')} />}
+                <p className={cx('desc')}>{card.description}</p>
+            </div>
         </div>
     );
 }
