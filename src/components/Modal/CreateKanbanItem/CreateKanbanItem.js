4a31344c47686dced5a8fd11314ca429dbb6ca54
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2022-11-02 15:28:53 +0700

    update UI createKanbanItem

--- src/components/Modal/CreateKanbanItem/CreateKanbanItem.js
+++ src/components/Modal/CreateKanbanItem/CreateKanbanItem.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import classNames from 'classnames/bind';
+import { AnimatePresence, motion } from 'framer-motion';
+
+import styles from './CreateKanbanItem.module.scss';
+import { BsXLg } from 'react-icons/bs';
+import { setCreateKanbanItemModalIsOpen } from '~/redux/Slice/modalSlice';
+
+const cx = classNames.bind(styles);
 
 function CreateKanbanItem() {
-    return <div>CreateKanbanItem</div>;
+    const { createKanbanItemModalIsOpen } = useSelector((state) => state.modal);
+
+    const handleCloseModal = () => {
+        setCreateKanbanItemModalIsOpen(false);
+    };
+
+    return (
+        <AnimatePresence>
+            <motion.div animate={{ width: createKanbanItemModalIsOpen ? '400px' : '0' }} className={cx('wrapper')}>
+                <header className={cx('header')}>
+                    <span className={cx('heading')}>
+                        Theme Customizer
+                        <button className={cx('close-btn')} onClick={handleCloseModal}>
+                            <BsXLg />
+                        </button>
+                    </span>
+                    <p className={cx('desc')}>Add item to the board</p>
+                </header>
+            </motion.div>
+        </AnimatePresence>
+    );
 }
 
 export default CreateKanbanItem;
