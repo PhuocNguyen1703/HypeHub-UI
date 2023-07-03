052d27a29ffd35174360a4da14867f6b297be387
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2023-07-04 01:06:42 +0700

    update validate change password

--- src/pages/Profile/components/ChangePassword/ChangePassword.js
+++ src/pages/Profile/components/ChangePassword/ChangePassword.js
@@ -3,6 +3,10 @@ import classNames from 'classnames/bind';
 
 import styles from './ChangePassword.module.scss';
 import { BsExclamationTriangle, BsEyeFill, BsEyeSlashFill } from 'react-icons/bs';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { HiCheck } from 'react-icons/hi';
 
 const cx = classNames.bind(styles);
 
@@ -10,29 +14,82 @@ function ChangePassword() {
     const [showOldPassword, setShowOldPassword] = useState(false);
     const [showNewPassword, setShowNewPassword] = useState(false);
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [lengthValidated, setLengthValidated] = useState(false);
+    const [lowerValidated, setLowerValidated] = useState(false);
+    const [upperValidated, setUpperValidated] = useState(false);
+    const [numberValidated, setNumberValidated] = useState(false);
+    const [specialValidated, setSpecialValidated] = useState(false);
+
+    const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+
+    const formSchema = yup.object().shape({
+        oldPassword: yup
+            .string()
+            .required('Please enter old password.')
+            .min(8, 'Please enter at least 8 characters.')
+            .matches(passwordRegExp, 'Password incorrect.')
+            .test('Password has spaces', 'Password without spaces.', (value) => !/\s+/.test(value)),
+        newPassword: yup
+            .string()
+            .required('Please enter new password.')
+            .min(8, 'New password does not match condition.')
+            .matches(passwordRegExp, 'New password does not match condition.')
+            .test('Password has spaces', 'Password without spaces.', (value) => !/\s+/.test(value)),
+        confirmPassword: yup
+            .string()
+            .required('Please enter confirm password')
+            .oneOf([yup.ref('newPassword')], 'Confirm password does not match.'),
+    });
+
+    const {
+        register,
+        formState: { errors, isSubmitting },
+        reset,
+        handleSubmit,
+    } = useForm({
+        mode: 'onBlur',
+        resolver: yupResolver(formSchema),
+    });
 
     const handleToggleShowOldPassword = () => {
         const inputEl = document.getElementById('old-password');
         setShowOldPassword((prevState) => !prevState);
-
-        if (inputEl.type === 'password') return (inputEl.type = 'text');
-        if (inputEl.type === 'text') return (inputEl.type = 'password');
+        checkTypeInputEl(inputEl);
     };
 
     const handleToggleShowNewPassword = () => {
         const inputEl = document.getElementById('new-password');
         setShowNewPassword((prevState) => !prevState);
-
-        if (inputEl.type === 'password') return (inputEl.type = 'text');
-        if (inputEl.type === 'text') return (inputEl.type = 'password');
+        checkTypeInputEl(inputEl);
     };
 
     const handleToggleShowConfirmPassword = () => {
         const inputEl = document.getElementById('confirm-password');
         setShowConfirmPassword((prevState) => !prevState);
+        checkTypeInputEl(inputEl);
+    };
 
+    const checkTypeInputEl = (inputEl) => {
         if (inputEl.type === 'password') return (inputEl.type = 'text');
-        if (inputEl.type === 'text') return (inputEl.type = 'password');
+        return (inputEl.type = 'password');
+    };
+
+    const handleCheckNewPassword = (value) => {
+        const length = new RegExp('(?=.{8,})');
+        const lower = new RegExp('(?=.*[a-z])');
+        const upper = new RegExp('(?=.*[A-Z])');
+        const number = new RegExp('(?=.*[0-9])');
+        const special = new RegExp('(?=.*[!@#$%^&*])');
+
+        setLengthValidated(length.test(value) ? true : false);
+        setLowerValidated(lower.test(value) ? true : false);
+        setUpperValidated(upper.test(value) ? true : false);
+        setNumberValidated(number.test(value) ? true : false);
+        setSpecialValidated(special.test(value) ? true : false);
+    };
+
+    const onSubmit = async (data) => {
+        console.log(data);
     };
 
     return (
@@ -40,62 +97,104 @@ function ChangePassword() {
             <header className={cx('header')}>
                 <span className={cx('title')}>Change Password</span>
             </header>
-            <form className={cx('form')}>
-                <div className={cx('form-left')}>
-                    <label className={cx('label')}>
-                        Old Password
-                        <input id="old-password" type="password" className={cx('input')} placeholder="Password" />
-                        <span className={cx('icon-eye')} onClick={handleToggleShowOldPassword}>
-                            {showOldPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
-                        </span>
-                    </label>
-                    <label className={cx('label')}>
-                        New Password
-                        <input id="new-password" type="password" className={cx('input')} placeholder="Password" />
-                        <span className={cx('icon-eye')} onClick={handleToggleShowNewPassword}>
-                            {showNewPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
-                        </span>
-                    </label>
-                    <label className={cx('label')}>
-                        Confirm Password
-                        <input id="confirm-password" type="password" className={cx('input')} placeholder="Password" />
-                        <span className={cx('icon-eye')} onClick={handleToggleShowConfirmPassword}>
-                            {showConfirmPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
-                        </span>
-                    </label>
-                    <div className={cx('action-btn')}>
-                        <button className={cx('cancel-btn')} type="button">
-                            Cancel
-                        </button>
-                        <button className={cx('save-btn')} type="submit">
-                            Save
-                        </button>
+            <div className={cx('container')}>
+                <form className={cx('form')} onSubmit={handleSubmit(onSubmit)}>
+                    <div className={cx('old-password')}>
+                        <div className={cx('old-password-field', errors.oldPassword && 'error-ipt')}>
+                            <label className={cx('label')}>Old password</label>
+                            <input
+                                id="old-password"
+                                type="password"
+                                className={cx('input')}
+                                placeholder="Enter old password..."
+                                {...register('oldPassword')}
+                            />
+                            <span className={cx('icon-eye')} onClick={handleToggleShowOldPassword}>
+                                {showOldPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
+                            </span>
+                        </div>
+                        {errors.oldPassword?.message && (
+                            <span className={cx('error')}>
+                                <BsExclamationTriangle className={cx('icon-warning')} />
+                                {errors.oldPassword?.message}
+                            </span>
+                        )}
                     </div>
-                </div>
-                <div className={cx('form-right')}>
+
+                    <div className={cx('new-password')}>
+                        <div className={cx('new-password-field', errors.newPassword && 'error-ipt')}>
+                            <label className={cx('label')}>New password</label>
+                            <input
+                                id="new-password"
+                                type="password"
+                                className={cx('input')}
+                                placeholder="Enter new password..."
+                                {...register('newPassword', {
+                                    onChange: (e) => handleCheckNewPassword(e.target.value),
+                                })}
+                            />
+                            <span className={cx('icon-eye')} onClick={handleToggleShowNewPassword}>
+                                {showNewPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
+                            </span>
+                        </div>
+                        {errors.newPassword?.message && (
+                            <span className={cx('error')}>
+                                <BsExclamationTriangle className={cx('icon-warning')} />
+                                {errors.newPassword?.message}
+                            </span>
+                        )}
+                    </div>
+
+                    <div className={cx('confirm-password')}>
+                        <div className={cx('confirm-password-field', errors.confirmPassword && 'error-ipt')}>
+                            <label className={cx('label')}>Confirm password</label>
+                            <input
+                                id="confirm-password"
+                                type="password"
+                                className={cx('input')}
+                                placeholder="Enter confirm password..."
+                                {...register('confirmPassword')}
+                            />
+                            <span className={cx('icon-eye')} onClick={handleToggleShowConfirmPassword}>
+                                {showConfirmPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
+                            </span>
+                        </div>
+                        {errors.confirmPassword?.message && (
+                            <span className={cx('error')}>
+                                <BsExclamationTriangle className={cx('icon-warning')} />
+                                {errors.confirmPassword?.message}
+                            </span>
+                        )}
+                    </div>
+
+                    <button className={cx('change-password-btn')} type="submit">
+                        Change Password
+                    </button>
+                </form>
+                <div className={cx('validate-condition')}>
                     <h3>New password must contain:</h3>
-                    <span>
-                        <BsExclamationTriangle className={cx('icon')} />
-                        At least 8 characters
+                    <span className={cx('condition', lengthValidated && 'validated')}>
+                        <span className={cx('icon')}>{lengthValidated ? <HiCheck /> : <BsExclamationTriangle />}</span>
+                        At least 8 characters.
                     </span>
-                    <span>
-                        <BsExclamationTriangle className={cx('icon')} />
-                        At least 1 lower letter (a-z)
+                    <span className={cx('condition', lowerValidated && 'validated')}>
+                        <span className={cx('icon')}>{lowerValidated ? <HiCheck /> : <BsExclamationTriangle />}</span>
+                        At least 1 lower letter. (a-z)
                     </span>
-                    <span>
-                        <BsExclamationTriangle className={cx('icon')} />
-                        At least 1 uppercase letter (A-Z)
+                    <span className={cx('condition', upperValidated && 'validated')}>
+                        <span className={cx('icon')}>{upperValidated ? <HiCheck /> : <BsExclamationTriangle />}</span>
+                        At least 1 uppercase letter. (A-Z)
                     </span>
-                    <span>
-                        <BsExclamationTriangle className={cx('icon')} />
-                        At least 1 number (0-9)
+                    <span className={cx('condition', numberValidated && 'validated')}>
+                        <span className={cx('icon')}>{numberValidated ? <HiCheck /> : <BsExclamationTriangle />}</span>
+                        At least 1 number. (0-9)
                     </span>
-                    <span>
-                        <BsExclamationTriangle className={cx('icon')} />
-                        At least 1 special characters
+                    <span className={cx('condition', specialValidated && 'validated')}>
+                        <span className={cx('icon')}>{specialValidated ? <HiCheck /> : <BsExclamationTriangle />}</span>
+                        At least 1 special character.
                     </span>
                 </div>
-            </form>
+            </div>
         </div>
     );
 }
